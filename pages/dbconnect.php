6c63d10376f9548ac6b1ec479e7e0e0450bb567b
hawww1705 <163385915+hawww1705@users.noreply.github.com>
2024-07-12 17:14:43 +0700

    Add files via upload

---
+++ pages/dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mahasiswa";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+   
